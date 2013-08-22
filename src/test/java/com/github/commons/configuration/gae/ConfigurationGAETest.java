70e2d209d6202fc4675bea26a50ffd7831f5fe50
Roberto Simoni <roberto.simoni@yahoo.it>
2013-08-22 17:51:42 +0200

    implementation of first test (DRAFT)

--- src/test/java/com/github/commons/configuration/gae/ConfigurationGAETest.java
+++ src/test/java/com/github/commons/configuration/gae/ConfigurationGAETest.java
@@ -1,19 +1,40 @@
 package com.github.commons.configuration.gae;
 
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.jmock.lib.concurrent.Synchroniser;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 
 import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
 public class ConfigurationGAETest {
 
-	@Rule public JUnitRuleMockery context = new JUnitRuleMockery();
+	@Rule public JUnitRuleMockery context = new JUnitRuleMockery() {{
+		setImposteriser(ClassImposteriser.INSTANCE);
+		setThreadingPolicy(new Synchroniser());
+	}};
 	private DatastoreService datastoreService = context.mock(DatastoreService.class);
 	private ConfigurationGAE configuration = new ConfigurationGAE(datastoreService);
 	
-	@Test public void xxx() {
-		Object value = configuration.getProperty("mykey");
+	@Test public void getProperty_uses_datastore_get_call_to_retrieve_an_entity_related_to_specified_configuration_parameter() throws EntityNotFoundException {
+		final String configurationParameter = "mykey";
+		final Key key = KeyFactory.createKey("Configuration", configurationParameter);
+		final Entity anEntity = new Entity(key);
+		
+		context.checking(new Expectations() {{ 
+			oneOf(datastoreService).get(with(any(Key.class)));
+				will(returnValue(anEntity));
+		}});
+		
+		Object value = configuration.getProperty(configurationParameter);
+		Assert.assertNotNull(value);
 	}
 
 }
