c1785ef879bb814ddb17ecfa636cd9b346e09359
Roberto Simoni <roberto.simoni@yahoo.it>
2013-08-23 12:15:00 +0200

    initial implementation with tests using local datastore provided by Google App Engine

--- src/test/java/com/github/commons/configuration/gae/ConfigurationGAETest.java
+++ src/test/java/com/github/commons/configuration/gae/ConfigurationGAETest.java
@@ -1,40 +1,98 @@
 package com.github.commons.configuration.gae;
 
-import org.jmock.Expectations;
-import org.jmock.integration.junit4.JUnitRuleMockery;
-import org.jmock.lib.concurrent.Synchroniser;
-import org.jmock.lib.legacy.ClassImposteriser;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.junit.After;
 import org.junit.Assert;
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
 public class ConfigurationGAETest {
 
-	@Rule public JUnitRuleMockery context = new JUnitRuleMockery() {{
-		setImposteriser(ClassImposteriser.INSTANCE);
-		setThreadingPolicy(new Synchroniser());
-	}};
-	private DatastoreService datastoreService = context.mock(DatastoreService.class);
-	private ConfigurationGAE configuration = new ConfigurationGAE(datastoreService);
+	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+	@Before public void setup() {
+		helper.setUp();
+	}
 	
-	@Test public void getProperty_uses_datastore_get_call_to_retrieve_an_entity_related_to_specified_configuration_parameter() throws EntityNotFoundException {
-		final String configurationParameter = "mykey";
-		final Key key = KeyFactory.createKey("Configuration", configurationParameter);
-		final Entity anEntity = new Entity(key);
+	@After public void teardown() {
+		helper.tearDown();
+	}
+	
+	private ConfigurationGAE configuration = new ConfigurationGAE(DatastoreServiceFactory.getDatastoreService());
+	
+	@Test public void getString_returns_previously_inserted_string() {
+		String aKey = "name";
+		String aValue = "John";
 		
-		context.checking(new Expectations() {{ 
-			oneOf(datastoreService).get(with(any(Key.class)));
-				will(returnValue(anEntity));
-		}});
+		configuration.setProperty(aKey, aValue);
+		Assert.assertEquals(aValue, configuration.getString(aKey));
+	}
+
+	@Test public void getInt_returns_previously_inserted_int() {
+		String aKey = "age";
+		int aValue = 37;
 		
-		Object value = configuration.getProperty(configurationParameter);
-		Assert.assertNotNull(value);
+		configuration.setProperty(aKey, aValue);
+		Assert.assertEquals(aValue, configuration.getInt(aKey));
+	}
+
+	@Test public void getString_returns_null_when_key_does_not_exist() {
+		Assert.assertNull(configuration.getString("unexistant key"));
+	}
+
+	@Test(expected=NoSuchElementException.class)
+	public void getString_throws_an_error_when_key_does_not_exist_and_configuration_is_set_to_thrown_errors_in_these_situations() {
+		configuration.setThrowExceptionOnMissing(true);
+		configuration.getString("unexistant key");
+	}
+
+	@Test public void containsKey_returns_true() {
+		String aKey = "name";
+		String aValue = "John";
+		
+		configuration.setProperty(aKey, aValue);
+		Assert.assertTrue(configuration.containsKey(aKey));
+	}
+
+	@Test public void containsKey_returns_false() {
+		String aKey = "name";
+		
+		Assert.assertFalse(configuration.containsKey(aKey));
+	}
+
+	@Test public void getKeys_returns_all_keys_previously_inserted() {
+		String key1 = "name";
+		String key2 = "lastname";
+		
+		configuration.setProperty(key1, "John");
+		configuration.setProperty(key2, "Johnson");
+		
+		assertIteratorContainsKey(configuration.getKeys(), key1);
+		assertIteratorContainsKey(configuration.getKeys(), key2);
+	}
+
+	@Test public void isEmpty_returns_true() {
+		Assert.assertTrue(configuration.isEmpty());
+	}
+	
+	@Test public void isEmpty_returns_false() {
+		configuration.setProperty("name", "John");
+		Assert.assertFalse(configuration.isEmpty());
+	}
+	
+	private void assertIteratorContainsKey(Iterator<String> keys, String key) {
+		while (keys.hasNext()) {
+			String currKey = keys.next();
+			if (currKey.equals(key))
+				return;
+		}
+		Assert.fail("key not found '" + key + "'");
 	}
 
 }
